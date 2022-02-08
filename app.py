389bb704eebbfebea281000225f84bfa4a049dce
Proggya <77157177+progite@users.noreply.github.com>
2022-02-08 08:43:16 +0530

    Stores outcome

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import socketserver
 import uuid
+from flaskext.mysql import MySQL
 from flask import Flask, request, render_template
 from flask_sock import Sock
+import database
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -12,22 +14,35 @@ last_player = dict()
 if __name__ == "__main__":
     app.run()
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+db = database.Manager(mysql=mysql)
+
 @sock.route("/play")
 def move(ws):
-    challenger = request.args.get("player")
+    challenger,white,black = request.json["challenger", "white", "black"]
     uid = request.args.get("gameId")
     websocket_pool[challenger] = ws
     
     while True:
-        move = ws.receive()
-        if last_player[uid] ==  challenger:
-            return "Error",400
-        
+        move,outcome = ws.receive().split(" ")
+        if outcome != 'NO':
+            break
+        if last_player[uid] == challenger:
+            return "Error", 400
+
+        db.store_moves(uid,move)
         last_player[uid] = challenger
         opponent = game_map[challenger]
         websocket_pool[opponent].send(move)
         return "", 200
 
+    db.store_outcome(uid,white, black,outcome)
+    return "",200
 
 @app.route("/challenge")
 def challenge():
@@ -35,11 +50,12 @@ def challenge():
     opponent = request.args.get("opponent_username")
     if opponent in game_map or challenger in game_map:
         return "Can't start a new game! One player is already playing a game", 400
-    
+
+    game_map[opponent] = challenger
+    game_map[challenger] = opponent
     global game_id
     game_id = str(uuid.uuid4())
     while game_id in last_player:
-        game_id = str(uuid.uuid4())    
+        game_id = str(uuid.uuid4())
     last_player[game_id] = opponent
-    return "", 201
-
+    return game_id, 201
