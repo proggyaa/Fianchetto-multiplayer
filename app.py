ea38c73fbb6b5f359e08e3097aee5cb2f77f20cb
Proggya <77157177+progite@users.noreply.github.com>
2022-02-11 00:47:29 +0530

    Fixed database issues

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from operator import le
 import socketserver
 import uuid
 from flaskext.mysql import MySQL
-from flask import Flask, request, render_template
+from flask import Flask, request
 from flask_sock import Sock
 import database
 
@@ -17,32 +18,38 @@ if __name__ == "__main__":
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_DB'] = 'fianchetto'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-db = database.Manager(mysql=mysql)
+db = database.Manager(mysql)
+
 
 @sock.route("/play")
 def move(ws):
-    challenger,white,black = request.json["challenger"],request.json["white"],request.json["black"]
-    uid = request.args.get("gameId")
+    challenger, white, black = request.args.get(
+        "challenger"), request.args.get("white"), request.args.get("black")
+    uid = request.args.get("game_id")
     websocket_pool[challenger] = ws
-    
+    outcome = 'NO'
+
     while True:
-        move,outcome = ws.receive().split(" ")
-        if outcome != 'NO':
-            break
+        move, outcome = ws.receive().split(" ")
         if last_player[uid] == challenger:
             return "Error", 400
 
-        db.store_moves(uid,move)
+        db.store_moves(uid, move)
         last_player[uid] = challenger
         opponent = game_map[challenger]
-        websocket_pool[opponent].send(move)
-        return "", 200
 
-    db.store_outcome(uid,white, black,outcome)
-    return "",200
+        if outcome != 'NO':
+            break
+        websocket_pool[opponent].send(move+" "+outcome)
+
+    del game_map[challenger]
+    del game_map[opponent]
+    db.store_outcome(uid, white, black, outcome)
+    return "", 200
+
 
 @app.route("/challenge")
 def challenge():
@@ -53,9 +60,6 @@ def challenge():
 
     game_map[opponent] = challenger
     game_map[challenger] = opponent
-    global game_id
-    game_id = str(uuid.uuid4())
-    while game_id in last_player:
-        game_id = str(uuid.uuid4())
+    game_id = str(uuid.uuid4())[:30]
     last_player[game_id] = opponent
     return game_id, 201
--- database.py
+++ database.py
@@ -1,26 +1,32 @@
 from flaskext.mysql import MySQL
 
+
 class Manager:
-    def __init__(self, mysql : MySQL) -> None:
+    def __init__(self, mysql: MySQL) -> None:
         self.mysql = mysql
         conn = self.mysql.connect()
         cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS game_overview(uid VARCHAR(30),white_player VARCHAR(20),black_player VARCHAR(20),outcome VARCHAR(8))''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS game_moves(uid VARCHAR(30),moves VARCHAR(5))''')
+        cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS game_overview(uid VARCHAR(20),white_player VARCHAR(20),black_player VARCHAR(20),outcome VARCHAR(500))''')
+        cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS game_moves(uid VARBINARY(30),moves VARCHAR(5))''')
         cursor.close()
         conn.close()
 
-
-    def store_moves(self, uid,move):
+    def store_moves(self, uid, move):
         conn = self.mysql.connect()
         cursor = conn.cursor()
-        cursor.execute(''' INSERT INTO game_moves VALUES(%s, %s)''',(uid,move))
+        cursor.execute(
+            ''' INSERT INTO game_moves VALUES(%s, %s)''', (uid, move))
+        conn.commit()
         cursor.close()
         conn.close()
 
-    def store_outcome(self,uid,white_player, black_player, outcome ):
+    def store_outcome(self, uid, white_player, black_player, outcome):
         conn = self.mysql.connect()
         cursor = conn.cursor()
-        cursor.execute(''' INSERT INTO game_overview VALUES(%s, %s, %s, %s)''',(uid,white_player,black_player,outcome))
+        cursor.execute(''' INSERT INTO game_overview VALUES(%s, %s, %s, %s)''',
+                       (uid, white_player, black_player, outcome))
+        conn.commit()
         cursor.close()
         conn.close()
